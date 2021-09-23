28cb5a75161afca6b3fc40627fbb8dc612690a0c
me@me.net <me@me.net>
2021-09-23 12:18:08 +0200

    parse ship and planet, add bodies to sim

--- step_2/g2d.c
+++ step_2/g2d.c
@@ -1,55 +1,191 @@
 #include <math.h>
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 /*2d gravity*/
 
+#define NAMELEN 16
 struct body {
-    double mass;    //kilograms
-    double radius;  //meters, circular body
-    double rx,ry;   //pos in meters
-    double vx,vy;   //speed in m/sec
-    double ax,ay;   //accels in m/sec^2
+    double  mass;       //kilograms
+    double  radius;     //meters, circular body
+    double  rx,ry;      //pos in meters
+    double  vx,vy;      //speed in m/sec
+    double  ax,ay;      //accels in m/sec^2
+    char    name[NAMELEN];
 };
 
 struct state {
-    struct body   *bodies;
-    int           bcount;
-    double        t;
-    unsigned long steps;
+    struct body     *bodies;
+    int             bcount;
+    double          t;      //current time
+    double          tmax;   //max sim duration
+    double          dt;     //time step
+    unsigned long   steps;  //step count
 };
 
 struct state sim;
 
 #define G 6.6743015E-11
 
-#define dt 1e-3
-#define time 8000
+/*---------------------------------------------------------------------------*/
+int sim_init(struct state *dest) {
+    dest->bodies = NULL;
+    dest->bcount = 0;
+    dest->tmax = 0;
+    dest->dt = 0;
+    return 0;
+}
 
+/*---------------------------------------------------------------------------*/
+int sim_end(struct state *dest) {
+    free(dest->bodies);
+    return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+int sim_start(struct state *dest) {
+    dest->t = 0;
+    dest->steps = 0;
+    return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+int sim_check(struct state *dest) {
+    printf("check: body count = %d\n", dest->bcount);
+    if(!dest->bcount) return 1; //no bodies
+    if(!dest->dt) return 1; //no timestep
+    if(!dest->tmax) return 1; //no max duration
+    return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+int sim_done(struct state *dest) {
+    return (dest->t >= dest->tmax);
+}
+
+/*---------------------------------------------------------------------------*/
+int sim_add_body(struct state *dest, char *name, double mass, double radius) {
+    dest->bodies = realloc(dest->bodies, sizeof(struct body) * (dest->bcount+1));
+    if(dest->bodies) {
+        memset(&dest->bodies[dest->bcount], 0, sizeof(struct body));
+        dest->bodies[dest->bcount].mass = mass;
+        dest->bodies[dest->bcount].radius = radius;
+        strncpy(dest->bodies[dest->bcount].name, name, NAMELEN);
+        dest->bcount += 1;
+        printf("sim: add body %s mass %g rad %g\n",name,mass,radius);
+        return 0;
+    }
+    return 1; //failed
+}
+
+/*---------------------------------------------------------------------------*/
+int sim_run(struct state *s) {
+    printf("[%lu] %g\n", s->steps, s->t);
+
+    //do it
+    s->t += s->dt;
+    s->steps += 1;
+    return 0;
+}
+
+/*---------------------------------------------------------------------------*/
 //[planet] name mass radius pos
 int parse_planet(struct state *dest, char *buf) {
+    double m,r;
+    char *name,*ebuf;
+    name = buf;
     printf("PLANET =>%s\n",buf);
-    return 0;
+    //find name end
+    while(*buf && *buf !=0x20) {
+        buf += 1;
+    }
+    ebuf = buf;
+    while(*buf && *buf==0x20) {
+        buf += 1;
+    }
+    if(!*buf) {
+        printf("missing planet mass");
+        return 1;
+    }
+    *ebuf = 0; //cut name
+    m=strtod(buf,&ebuf);
+    buf = ebuf;
+    while(*buf && *buf==0x20) {
+        buf += 1;
+    }
+    if(!*buf) {
+        printf("missing planet radius");
+        return 1;
+    }
+    r=strtod(buf,&ebuf);
+    buf = ebuf;
+    while(*buf && *buf==0x20) {
+        buf += 1;
+    }
+    if(*buf) {
+        printf("warning: spurious planet info: %s\n",buf);
+    }
+
+    return sim_add_body(dest,name,m,r);
 }
 
+/*---------------------------------------------------------------------------*/
 //[ship] name mass radius [around] planet alt angle
 int parse_ship(struct state *dest, char *buf) {
+    double m,r;
+    char *name,*ebuf;
+    name = buf;
     printf("SHIP =>%s\n",buf);
-    return 0;
+    //find name end
+    while(*buf && *buf !=0x20) {
+        buf += 1;
+    }
+    ebuf = buf;
+    while(*buf && *buf==0x20) {
+        buf += 1;
+    }
+    if(!*buf) {
+        printf("missing ship mass");
+        return 1;
+    }
+    *ebuf = 0; //cut name
+    m=strtod(buf,&ebuf);
+    buf = ebuf;
+    while(*buf && *buf==0x20) {
+        buf += 1;
+    }
+    if(!*buf) {
+        printf("missing ship radius");
+        return 1;
+    }
+    r=strtod(buf,&ebuf);
+    buf = ebuf;
+    while(*buf && *buf==0x20) {
+        buf += 1;
+    }
+    if(*buf) {
+        printf("warning: spurious ship info: %s\n",buf);
+    }
+
+    return sim_add_body(dest,name,m,r);
 }
 
+/*---------------------------------------------------------------------------*/
 //[sim] timestep duration
 int parse_sim(struct state *dest, char *buf) {
     printf("SIM =>%s\n",buf);
     return 0;
 }
 
+/*---------------------------------------------------------------------------*/
 //[plot] body param ref
 int parse_plot(struct state *dest, char *buf) {
     printf("PLOT =>%s\n",buf);
     return 0;
 }
 
+/*---------------------------------------------------------------------------*/
 int parse_line(struct state *dest, char *buf) {
     char *inst;
 
@@ -63,7 +199,7 @@ int parse_line(struct state *dest, char *buf) {
         *buf = 0; //cut instruction
         buf += 1; //skip after
         //skip all spaces
-        while(*buf && *buf ==0x20) {
+        while(*buf && *buf==0x20) {
             buf += 1;
         }
     }
@@ -83,6 +219,7 @@ int parse_line(struct state *dest, char *buf) {
     return 1;
 }
 
+/*---------------------------------------------------------------------------*/
 int parse(struct state *dest, const char *fname) {
     FILE *f;
     char buf[256];
@@ -132,12 +269,29 @@ int parse(struct state *dest, const char *fname) {
     return 0;
 }
 
+/*---------------------------------------------------------------------------*/
 int main(int argc, char **argv) {
     if(argc != 2) {
         printf("%s <simfile>\n", argv[0]);
         return 1;
     }
+    sim_init(&sim);
+
     parse(&sim, argv[1]);
+
+    if(sim_check(&sim)) {
+        printf("simulation failed check\n");
+        return 1;
+    }
+
+    printf("simulation start\n");
+    sim_start(&sim);
+    while(!sim_done(&sim)) {
+        sim_run(&sim);
+    }
+    sim_end(&sim);
+    printf("simulation done\n");
+    return 0;
 }
 
 #if 0
